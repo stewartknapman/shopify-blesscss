a3edacd664084607117ffbe86fe84d4784c4cce9
Stewart Knapman <info@stewartknapman.com>
2015-11-10 18:29:03 +1300

    pull and bless css files

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 var fs = require('fs');
 var yaml = require('js-yaml');
+var bless = require('bless');
 var request = require('request');
 
-var Blessify = function (config_path, stylesheet_name) {
+var Blessify = function (config_path, stylesheet_name, output_path) {
   this.stylesheet_name = stylesheet_name;
+  this.output_path = output_path;
   this.config = this._get_config(config_path);
   this.asset_url = this._build_asst_url();
+
+  this.bless = bless.Parser({ output: '', options: {}});
   
-  var _this = this;
+  // find the correct asset path so we can find the stylesheet in question.
   this.request_data(this.asset_url, function (data) {
     data = JSON.parse(data); // asset json data
-    var asset_path = _this._get_asset_path(data.assets[0].public_url);
+    var asset_path = this._get_asset_path(data.assets[0].public_url);
     
-    console.log(asset_path);
+    // Now we have the asset path lets get the stylesheet
+    this._request_stylesheet(asset_path);
   });
 };
 
@@ -44,9 +49,23 @@ Blessify.prototype._get_asset_path = function (path) {
   var path_parts = path.split('/');
   for (var i = 0; i < path_parts.length - 1; i++) {
     asset_path += path_parts[i] + '/';
-//     asset_path += (i == 0)? '//' : '/';
   }
   return asset_path;
+};
+
+Blessify.prototype._request_stylesheet = function (asset_path) {
+  var style_url = asset_path + this.stylesheet_name;
+  this.request_data(style_url, function (data) {
+    this._bless_stylesheet(data);
+  });
+};
+
+Blessify.prototype._bless_stylesheet = function (css) {
+  var blessed = this.bless.parse(css, function (err, files, numSelectors) {
+    
+    console.log(numSelectors, files.length);
+    
+  });
 };
 
 module.exports = Blessify;
--- run.js
+++ run.js
@@ -2,5 +2,6 @@ var Blessify = require('./index.js');
 
 var config_path = 'config.yml';
 var stylesheet_name = 'styles.scss.css';
+var output_path = './css';
 
-new Blessify(config_path, stylesheet_name);
+new Blessify(config_path, stylesheet_name, output_path);
